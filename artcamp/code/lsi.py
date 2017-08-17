3e74979260f485661ab1410093d83d0f3fb96cbe
jwilber <jwilber@classy.org>
2017-08-17 16:19:43 -0700

    Added index to save/load for lsi.py

--- artcamp/code/lsi.py
+++ artcamp/code/lsi.py
@@ -2,92 +2,110 @@
 import os
 
 from sklearn.base import BaseEstimator, TransformerMixin
+from utils import load_json
+
+name_list = load_json('../data/name_list.txt')
 
 
+# keep this
 class GensimLsi(BaseEstimator, TransformerMixin):
     """
-    Custom sklearn transformer for LSI.
-
-    Wrapped by sklearn.
+    Custom sklearn transformer to convert tokenized,
+    preprocessed data to tf-idf representation.
     """
-
-    def __init__(self, lsi_path=None):
-        """
-        Instantiate GensimLsi object.
-
-        Parameters
-        ----------
-        lsi_path : str
-            Path to location of saved gensim LsiModel.
-            If specified, the model will load and use this object
-            as its LsiModel.
-        """
-        self.model = None
-        self.id2word = None
-        self.num_topics = None
+    def __init__(self, id2word_path, lsi_path=None, index_path=None):
+        from gensim.corpora.dictionary import Dictionary
+        self.num_topics = 10
         self.corpus = None
         self.index = None
+        self.model = None
+        self.id2word = self.load(id2word_path=id2word_path)
         if lsi_path is not None:
-            self.load(lsi_path)
-
+            self.model = self.load(lsi_path=lsi_path)
+        if index_path is not None:
+            self.index = self.load(index_path=index_path)
+        
     @staticmethod
-    def load(lsi_path):
+    def load(lsi_path=None, id2word_path=None, index_path=None):
         """
-        Load previously saved LSI model.
+        If specified, attempts to load gensim LsiModel from `lsi_path`
+        and gensim Dictionary from `dictionary_path`.
 
         Parameters
         ----------
         lsi_path: str
             File-path designating where self.model should be saved.
+        id2word_path: str
+            File-path designating where self.dictionary should be saved.
         """
-        import pickle
-        if not os.path.exists(lsi_path):
-            raise IOError('File path to the LsiModel was not found.'
-                          'Please insert correct path to LsiModel.')
-        with file(lsi_path, 'rb') as f:
-            return pickle.load(f)
+        if lsi_path is not None:
+            from gensim.models import LsiModel
+            if not os.path.exists(lsi_path):
+                raise IOError('The provided file path to the LsiModel was not found.'
+                              'Please ensure that the argument is the correct path.')
+            return LsiModel.load(lsi_path)
+        if id2word_path is not None:
+            from gensim.corpora.dictionary import Dictionary
+            if not os.path.exists(id2word_path):
+                raise IOError('The provided file path to the Dictionary was not found.'
+                              'Please ensure that the argument is the correct path.')
+            return Dictionary.load(id2word_path)
+        if index_path is not None:
+            from gensim.similarities import MatrixSimilarity
+            if not os.path.exists(index_path):
+                raise IOError('The provided file path to the Dictionary was not found.'
+                              'Please ensure that the argument is the correct path.')
+            return MatrixSimilarity.load(index_path)
 
-    def save(self, lsi_path):
+    def save(self, lsi_path=None, id2word_path=None, index_path=None):
         """
-        Save model to disc.
+        Saves objects from fit process: gensim.LsiModel to `lsi_path`
+        and gensim.Dictionary to `dictionary_path`.
+        If either self.model or self.dictionary does not exist, an
+        AttributeError is raised.
 
         Parameters
         ----------
         lsi_path: str
             File-path designating where self.model should be saved.
+        dictionary_path: str
+            File-path designating where self.dictionary should be saved.
         """
-        import pickle
-        if not (self.model):
+        if not (self.model and self.id2word):
             raise AttributeError('Nothing to save yet, please run .fit first.')
-        f = file(lsi_path, 'wb')
-        pickle.dump(self, f, pickle.HIGHEST_PROTOCOL)
-        f.close()
+        if lsi_path is not None:   
+            self.model.save(lsi_path)
+        if id2word_path is not None:
+            self.id2word.save(id2word_path)
+        if index_path is not None:
+            self.index.save(index_path)
+        
+        
 
-    def fit(self, documents, id2word, num_topics=10, labels=None):
+    def fit(self, documents, num_topics=600, labels=None):
         """
-        Fit LsiModel to documents.
+        Fits a gensim LsiModel to documents.
 
         Parameters
         ----------
         documents: iterable
-            List of documents, each itself a list of preprocessed tokens.
-        labels: iterable (default=None)
-            Optional list specifying document labels.
+            List of documents. Each document must be a list of preprocessed tokens.
+        labels: iterable
+            Optional list of same size as documents, specifying label for each document.
 
         """
         from gensim.models import LsiModel
+        from gensim.corpora.dictionary import Dictionary
         if self.id2word is None:
-            self.id2word = id2word
+            raise AttributeError("Must specify id2word.")
         self.num_topics = num_topics
-        self.model = LsiModel(corpus=documents,
-                              id2word=id2word,
-                              num_topics=num_topics)
+        self.model = LsiModel(documents, id2word=self.id2word, num_topics=600)
         self.corpus = self.model[documents]
         return self
 
     def transform(self, documents):
         """
-        Return ectorized embedding for each document in documents.
+        Returns a vectorized embedding of each document in documents.
 
         Parameters
         -----------
@@ -102,23 +120,25 @@ class GensimLsi(BaseEstimator, TransformerMixin):
             raise AttributeError('Must have a fit model in order'
                                  ' to call transform.')
         return self.model[documents]
-
-    def similarity(self, org, n=10):
+    
+    def similarity(self, doc, n=10):
         """
-        Return n most similar orgs to org.
-
+        Returns the `n` most similar items in `self.corpus`
+        to `doc`.
+        
         Parameters
         ----------
-        org:
+        doc:
             A document. embedded in same tfidf space as model.
         n: int (default=10)
             Number of most similar items to return.
-
+            
         Returns
         -------
             sims: dictionary of (item, distance) key, value pairs sorted by
                 similarity in descending order.
         """
+        _n = n
         if self.model is None:
             raise AttributeError('Must have a fit model in order'
                                  ' to call similarity.')
@@ -127,12 +147,15 @@ class GensimLsi(BaseEstimator, TransformerMixin):
             print
             from gensim.similarities import MatrixSimilarity
             self.index = MatrixSimilarity(self.corpus)
+            
         # if n is larger than the number of documents, return 1 result
         _n = n * (n < len(self.index)) + (1 - (n < len(self.index))) * 1
-
+                                     
         if _n == 1:
-            print("{n} too large a number, returning 1 result.").format(n=n)
-        doc_lsi = self.model[org]
+            print("{n} too large a number, returning 1 result instead.").format(n=n)
+        doc_lsi = self.model[doc]
         sims = self.index[doc_lsi]
         sims = sorted(enumerate(sims), key=lambda item: -item[1])[0:_n]
+        # results = ["Org: {0: <36} Similarity: {s}".format(data_names[i].encode('utf8').strip(), s=j) 
+        #            for i, j in name_list]
         return sims
