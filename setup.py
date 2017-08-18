8bd1968ab96aa5205a9994c425a73c35549ddaba
jwilber <jwilber@classy.org>
2017-08-17 18:15:50 -0700

    data

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='artcamp',
-      version='0.16',
+      version='0.161',
       description='Query similarity between articles, orgs, and/or campaigns',
       url='http://github.com/jwilber/artcamp',
       author='Jared Wilber',
