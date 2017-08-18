ce3e75f8fdb523136598898e257aae3035ad8d3f
jwilber <jwilber@classy.org>
2017-08-17 18:09:22 -0700

    Added data flag

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ def readme():
 
 
 setup(name='artcamp',
-      version='0.157',
+      version='0.158',
       description='Query similarity between articles, orgs, and/or campaigns',
       url='http://github.com/jwilber/artcamp',
       author='Jared Wilber',
       author_email='jwilber@classy.org',
+      package_data={'artcamp': 'data/*'},
       include_package_data=True,
       license='MIT',
       packages=['artcamp'],
